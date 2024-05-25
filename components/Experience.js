2e1f05df0b890d1c87f57a60b3ca19901822b2e4
Mohamed Taher MAALEJ <mohamedtahermaalej@gmail.com>
2024-05-25 23:16:03 +0200

    feat: Add LiIcon & Experience Components

--- components/Experience.js
+++ components/Experience.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useScroll } from 'framer-motion'
+import LiIcon from './LiIcon'
 
 
 const Details = ({position, company, companyLink, duration, address, work}) => {
+  const ref = useRef(null)
   return (
-    <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-      <div>
+    <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+      <LiIcon reference={ref} />
+      <motion.div
+      initial={{y: 50}}
+      whileInView={{y: 0}}
+      transition={{duration: 0.5, type: "spring"}}
+      
+      >
         <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink}
         target={'_blank'}
         className='text-primary capitalize'>@{company}</a></h3>
@@ -15,20 +24,31 @@ const Details = ({position, company, companyLink, duration, address, work}) => {
           {work}
         </p>
 
-      </div>
+      </motion.div>
     </li>
 
   )
 }
 
 function Experience() {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll(
+    {
+      target: ref,
+      offset: ["start end", "center start"]
+    }
+  )
+
   return (
     <div className='my-64'>
       <h2 className='font-bold text-8xl mb-32 w-full text-center'>
         Experience
       </h2>
-      <div className='w-[75%] mx-auto relative'>
-        <ul>
+      <div ref={ref} className='w-[75%] mx-auto relative'>
+        <motion.div style={{scaleY: scrollYProgress}} className='absolute top-0 left-9 z-[4px] h-full bg-black origin-top '/>
+
+
+        <ul className='w-full flex flex-col items-start justify-between ml-4'>
           <Details  position='Software Engineer' company='Company 1' companyLink='https://www.company1.com' duration='2020 - Present' address='City 1, Country 1' work='Work 1'/>
           <Details  position='Software Engineer' company='Company 2' companyLink='https://www.company2.com' duration='2019 - 2020' address='City 2, Country 2' work='Work 2'/>
           <Details  position='Software Engineer' company='Company 3' companyLink='https://www.company3.com' duration='2018 - 2019' address='City 3, Country 3' work='Work 3'/>
---
+++ components/LiIcon.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { motion, useScroll } from 'framer-motion'
+
+function LiIcon({ reference }) {
+
+  const { scrollYProgress } = useScroll(
+    {
+      target: reference,
+      offset: ["center end", "center center"]
+    }
+  )
+
+  return (
+    <figure className='absolute left-0 stroke-black'>
+      <svg className='-rotate-90' width='75' height='75' viewBox='0 0 100 100'>
+        <circle cx='75' cy='50' r='20' className='stroke-red-600 stroke-1 fill-none'/>
+        <motion.circle cx='75' cy='50' r='20' className='stroke-[5px] fill-white' style={{
+          pathLength: scrollYProgress
+        }}/>
+        <circle cx='75' cy='50' r='10' className='animate-pulse stroke-1 fill-red-600'/>
+      </svg>
+    </figure>
+  )
+}
+
+export default LiIcon
