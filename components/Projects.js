c302d6076b470456c7f8479d39959a04f21c04c1
Mohamed Taher MAALEJ <mohamedtahermaalej@gmail.com>
2024-06-02 23:13:03 +0200

    feat: Create Projects Page

---
+++ components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+function Projects() {
+  return (
+    <>
+        <Head>
+            <title>Mohamed Taher MAALEJ | Projects Page</title>
+            <meta name="description" content="Mohamed Taher MAALEJ's Projects Page" />
+        </Head>
+    </>
+  )
+}
+
+export default Projects
