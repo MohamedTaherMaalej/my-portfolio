276420ce9da95232530f6f277d6bfe548c96afa1
MohamedTaherMaalej <mohamedtahermaalej@gmail.com>
2025-03-02 15:14:51 +0000

    Update Skills Component

--- components/Skills.js
+++ components/Skills.js
@@ -20,30 +20,24 @@ function Skills() {
     <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32'>Skills</h2>
     <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] md:h-[60vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd sm:bg-circularLightSm sm:dark:bg-circularDarkSm'>
       <motion.div className=' flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2' whileHover={{scale: 1.05}}>
-        ICT
+        Cybersecurity
       </motion.div>
-      <Skill name='HTML' x='-5vw' y='-10vw'/>
-      <Skill name='CSS' x='5vw' y='-10vw'/>
-      <Skill name='JavaScript' x='0' y='-20vw'/>
-      <Skill name='Next.js' x='-5vw' y='10vw'/>
+      <Skill name='SQL' x='-5vw' y='-10vw'/>
+      <Skill name='Java' x='5vw' y='-10vw'/>
+      <Skill name='Windows' x='0' y='-20vw'/>
+      <Skill name='JavaScript' x='-5vw' y='10vw'/>
       <Skill name='Python' x='5vw' y='10vw'/>
-      <Skill name='C++' x='0' y='20vw'/>
+      <Skill name='Linux' x='0' y='20vw'/>
       <Skill name='C' x='-10vw' y='0'/>
-      <Skill name='Java' x='10vw' y='0'/>
+      <Skill name='C++' x='10vw' y='0'/>
       <Skill name='Nmap' x='15vw' y='15vw'/>
       <Skill name='Burp Suite' x='-15vw' y='15vw'/>
       <Skill name='Wireshark' x='15vw' y='-15vw'/>
       <Skill name='Metasploit' x='-15vw' y='-15vw'/>
-      <Skill name='SQL' x='20vw' y='0'/>
+      <Skill name='John' x='20vw' y='0'/>
       <Skill name='Netcat' x='-20vw' y='0'/>
-      <Skill name='Linux' x='0' y='30vw'/>
-      <Skill name='Windows' x='0' y='-30vw'/>
-      <Skill name='Hydra' x='25vw' y='25vw'/>
-      <Skill name='John the Ripper' x='-25vw' y='25vw'/>
-      <Skill name='Kali Linux' x='25vw' y='-25vw'/>
-      <Skill name='Ubuntu' x='-25vw' y='-25vw'/>
-      <Skill name='Git' x='30vw' y='0'/>
-      <Skill name='GitHub' x='-30vw' y='0'/>
+      <Skill name='CI/CD' x='30vw' y='0'/>
+      <Skill name='Git' x='-30vw' y='0'/>
 
     </div>
     </>
