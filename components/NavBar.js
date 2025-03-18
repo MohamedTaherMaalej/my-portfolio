2c88e34cfe1035d987c4c234fe5d8fab49fc8479
MohamedTaherMaalej <mohamedtahermaalej@gmail.com>
2025-03-18 19:40:07 +0000

    feat: Add X URL

--- components/NavBar.js
+++ components/NavBar.js
@@ -60,7 +60,7 @@ function NavBar() {
               <CustomLink href='/articles' title="Articles" className='mx-4'/>
           </nav>
           <nav className='flex items-center justify-center flex-wrap'>
-              <motion.a href='https://twitter.com/' target={'_blank'} whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mr-3'><TwitterIcon /> </motion.a>
+              <motion.a href='https://x.com/medtaher_maalej' target={'_blank'} whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mr-3'><TwitterIcon /> </motion.a>
               <motion.a href='https://github.com/MohamedTaherMaalej' target={'_blank'} whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mx-3'><GithubIcon /></motion.a>
               <motion.a href='https://www.linkedin.com/in/mohamed-taher-maalej' target={'_blank'} whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mx-3'><LinkedInIcon /></motion.a>
               <motion.a href='https://www.linkedin.com/in/mohamed-taher-maalej' target={'_blank'} whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mx-3 bg-light rounded-full'><PinterestIcon /></motion.a>
